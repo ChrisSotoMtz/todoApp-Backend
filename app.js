95fa9b23bb4d4050d0bb80716f036f0880611025
Christian Soto <syasdras@gmail.com>
2023-11-15 17:13:56 -0600

    Added cors policy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 const indexRouter = require('./routes/index');
 
 var app = express();
@@ -12,6 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
 
 app.use('/', indexRouter);
 module.exports = app;
